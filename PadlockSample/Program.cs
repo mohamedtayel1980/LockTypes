fbc0749c054e7b5bc5b108481a31a1975042bc6e
mohamed.tayel <moh_moh701@hotmail.com>
2021-02-26 09:26:43 +0200

    Add project files.

---
+++ PadlockSample/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PadlockSample
+{
+    class BankAccount
+    {
+        public object padlock = new object();
+        private int _balance;
+
+        public int Balance { get => _balance; private set => _balance = value; }
+
+        public void Deposit(int amount)
+        {
+            // += is really two operations
+            // op1 is temp <- get_Balance() + amount
+            // op2 is set_Balance(temp)
+            // something can happen _between_ op1 and op2
+            Interlocked.Add(ref _balance, amount);
+
+            //p1
+            //p2
+            Interlocked.MemoryBarrier();
+            //p3
+
+
+
+        }
+
+        public void Withdraw(int amount)
+        {
+            Interlocked.Add(ref _balance, -amount);
+              
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasks = new List<Task>();
+            var ba = new BankAccount();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; ++j)
+                        ba.Deposit(100);
+                }));
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; ++j)
+                        ba.Withdraw(100);
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine($"Final balance is {ba.Balance}.");
+
+
+            Console.WriteLine("All done here.");
+        }
+    }
+}
---
+++ SpainLockSample/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpainLockSample
+{
+    class BankAccount
+    {
+        public object padlock = new object();
+        public int Balance { get; private set; }
+
+        public void Deposit(int amount)
+        {
+
+
+
+            Balance += amount;
+
+        }
+
+        public void Withdraw(int amount)
+        {
+
+            Balance -= amount;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasks = new List<Task>();
+            var ba = new BankAccount();
+            SpinLock spinLock = new SpinLock();
+            Console.WriteLine("spinLock started");
+            for (int i = 0; i < 10; ++i)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; ++j)
+                    {
+                        var lockTaken = false;
+                        try
+                        {
+                            spinLock.Enter(ref lockTaken);
+                            ba.Deposit(100);
+                        }
+                        finally
+                        {
+                            if (lockTaken) spinLock.Exit(true);
+                        }
+                        
+                          
+                        
+                        
+                    }
+                }));
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; ++j)
+                    {
+                        var lockTaken = false;
+                        try
+                        {
+                          
+                            spinLock.Enter(ref lockTaken);
+                            ba.Withdraw(100);
+                        }
+                        finally
+                        {
+                            if (lockTaken) spinLock.Exit(true);
+                        }
+                       
+                        
+                       
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine($"Final balance is {ba.Balance}.");
+            Console.WriteLine("spinLock End");
+
+            Console.WriteLine("All done here.");
+        }
+    }
+}
